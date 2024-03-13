c5b876c82f917e9e17a96d2e0076a2c7fa0d2beb
AgustinJaime <ajaime.dev@gmail.com>
2024-03-13 18:16:58 +0100

    css variables - header - progress bar

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import ProgressBar from "./components/ProgressBar";
 function App() {
   return (
     <div className="app-container">
-      <header className="app-header"></header>
+      <header className="app-header">
+        <h1>Lodgify Grouped Tasks</h1>
+      </header>
       <ProgressBar />
     </div>
   );
--- src/components/ProgressBar.tsx
+++ src/components/ProgressBar.tsx
@@ -1,5 +1,14 @@
+import "./ProgressBar.css";
 const ProgressBar = () => {
-  return <div>Progress Bar</div>;
+  return (
+    <div className="progress-bar-ctn">
+      <div className="progress-bar-base">
+        <div className="progress-bar-inner" style={{ width: "30%" }}>
+          18%
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProgressBar;
